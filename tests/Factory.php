f5569c49782889ce384f4acce0105499c2f21e00
Alexander Ustimenko <ustimenko.alexander@gmail.com>
2018-05-17 12:22:08 +0600

    Test ESIA access token

--- tests/Factory.php
+++ tests/Factory.php
@@ -2,7 +2,29 @@
 
 namespace Ekapusta\OAuth2Esia\Tests;
 
+use Ekapusta\OAuth2Esia\Token\EsiaAccessToken;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+
 class Factory
 {
     const KEYS = __DIR__.'/../resources/';
+
+    /**
+     * @return EsiaAccessToken
+     */
+    public static function createAccessToken($privateKeyPath, $publicKeyPath = null)
+    {
+        $accessToken = (new Builder())
+            ->setIssuedAt(time())
+            ->setNotBefore(time())
+            ->setExpiration(time() + 3600)
+            ->set('urn:esia:sbj_id', 1)
+            ->set('scope', 'one?oid=123 two?oid=456 three?oid=789')
+            ->sign(new Sha256(), new Key(file_get_contents($privateKeyPath)))
+            ->getToken();
+
+        return new EsiaAccessToken(['access_token' => (string) $accessToken], $publicKeyPath);
+    }
 }
---
+++ tests/Token/EsiaAccessTokenTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Ekapusta\OAuth2Esia\Tests\Token;
+
+use Ekapusta\OAuth2Esia\Tests\Factory;
+use Ekapusta\OAuth2Esia\Token\EsiaAccessToken;
+use PHPUnit\Framework\TestCase;
+
+class EsiaAccessTokenTest extends TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Access token is invalid
+     */
+    public function testInvalidAsItExpired()
+    {
+        new EsiaAccessToken([
+            'access_token' => file_get_contents(__DIR__.'/../Fixtures/expired.token.txt'),
+        ]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Access token can not be verified
+     */
+    public function testInvalidAsBadSignature()
+    {
+        Factory::createAccessToken(
+            Factory::KEYS.'ekapusta.rsa.test.key',
+            Factory::KEYS.'another.rsa.test.cer'
+        );
+    }
+
+    public function testFullyValid()
+    {
+        $esiaToken = Factory::createAccessToken(
+            Factory::KEYS.'ekapusta.rsa.test.key',
+            Factory::KEYS.'ekapusta.rsa.test.cer'
+        );
+
+        $this->assertInstanceOf(EsiaAccessToken::class, $esiaToken);
+
+        return $esiaToken;
+    }
+
+    /**
+     * @depends testFullyValid
+     */
+    public function testScopesExtracted(EsiaAccessToken $token)
+    {
+        $this->assertEquals(['one', 'two', 'three'], $token->getScopes());
+    }
+}
